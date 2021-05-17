14448fbcb94efc3fab09e20640d6224c89842161
juani <juaniv8@hotmail.com>
2021-05-17 21:02:25 +0100

    fix typo

--- src/theme/theme.jsx
+++ src/theme/theme.jsx
@@ -1,9 +1,26 @@
 import createBreakpoints from "@material-ui/core/styles/createBreakpoints";
 
+import WorkSansTTF from "../assets/fonts/WorkSans-VariableFont_wght.ttf";
+import RobotoTTF from "typeface-roboto";
+
 const Roboto = {
   fontFamily: "Roboto",
   fontStyle: "normal",
   fontWeight: 400,
+};
+
+const WorkSans = {
+  fontFamily: "Work Sans Thin",
+  fontStyle: "normal",
+  fontDisplay: "swap",
+  fontWeight: 400,
+  src: `
+    local('Work Sans Thin'),
+    local('Work Sans Thin'),
+    url(${WorkSansTTF}) format('truetype')
+  `,
+  unicodeRange:
+    "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF",
 };
 
 export const colors = {
