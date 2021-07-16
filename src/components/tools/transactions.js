cad3da994a27272b6dc2d3c150dcf1ebbaad6cd6
juani <juaniv8@hotmail.com>
2021-07-16 13:05:54 +0100

    TX list improve, almost done

--- src/components/tools/transactions.js
+++ src/components/tools/transactions.js
@@ -18,6 +18,13 @@ import {
   ListItemText,
   ListItemSecondaryAction,
   LinearProgress,
+  Table,
+  TableHead,
+  TableRow,
+  TableBody,
+  TableCell,
+  TableSortLabel,
+  TableContainer,
 } from "@material-ui/core";
 
 import {
@@ -46,6 +53,9 @@ import ArrowBackIosRoundedIcon from "@material-ui/icons/ArrowBackIosRounded";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import BackspaceRoundedIcon from "@material-ui/icons/BackspaceRounded";
 import RefreshRoundedIcon from "@material-ui/icons/RefreshRounded";
+
+import KeyboardArrowRightRoundedIcon from "@material-ui/icons/KeyboardArrowRightRounded";
+import KeyboardArrowLeftRoundedIcon from "@material-ui/icons/KeyboardArrowLeftRounded";
 
 import Store from "../../stores";
 const emitter = Store.emitter;
@@ -76,6 +86,16 @@ const styles = (theme) => ({
     display: "flex",
     flex: 1,
   },
+  card: {
+    background: "rgba(255,255,255,0.05)",
+    padding: 10,
+    margin: 10,
+  },
+  tokenLogo: {
+    maxHeight: 30,
+    minWidth: 30,
+    marginTop: 3,
+  },
 });
 
 class Transactions extends Component {
@@ -93,6 +113,8 @@ class Transactions extends Component {
       error: false,
       errMsgWallet: "",
       errorWallet: true,
+      sortBy: "block_signed_at",
+      sortOrder: "desc",
     };
     if (account && account.address) {
       dispatcher.dispatch({
@@ -425,6 +447,101 @@ class Transactions extends Component {
     }
   };
 
+  sortBy(_sortBy) {
+    let _prevSortBy = this.state.sortBy;
+    if (_prevSortBy === _sortBy) {
+      if (this.state.sortOrder === "asc") {
+        this._isMounted &&
+          this.setState({ sortBy: _sortBy, sortOrder: "desc" });
+      } else {
+        this._isMounted && this.setState({ sortBy: _sortBy, sortOrder: "asc" });
+      }
+    } else {
+      this._isMounted && this.setState({ sortBy: _sortBy, sortOrder: "desc" });
+    }
+  }
+
+  drawTransactions = (data) => {
+    const { classes } = this.props;
+    const { sortBy, sortOrder } = this.state;
+
+    function dynamicSort(property) {
+      var sortOrder = 1;
+      if (property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+      }
+      return function (a, b) {
+        var result =
+          a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
+
+        return result * sortOrder;
+      };
+    }
+
+    let allTX = [];
+    let sortedTXs = [];
+    // MERGE all transactions from all tokens in user wallet
+    // Create new empty array where to store all the TX
+    // Append to this array Asset specific data (Name, contract, ticker, and logo url)
+    // Push Tx to this new array, then sort by block_signed_at (date of TX)
+    data.assets.forEach((item, i) => {
+      item.txs.forEach((tx, j) => {
+        var d = new Date(tx.block_signed_at);
+        let dformat =
+          [d.getMonth() + 1, d.getDate(), d.getFullYear()].join("/") +
+          " " +
+          [d.getHours(), d.getMinutes(), d.getSeconds()].join(":");
+
+        tx.contract_name = item.contract_name;
+        tx.contract_address = item.contract_address;
+        tx.contract_ticker = item.contract_ticker;
+        tx.logo_url = item.logo_url;
+        tx.dateFormated = dformat.toString();
+        allTX.push(tx);
+      });
+    });
+
+    if (this.state.sortOrder === "asc") {
+      sortedTXs = allTX.sort(dynamicSort(sortBy));
+    } else {
+      sortedTXs = allTX.sort(dynamicSort(`-${sortBy}`));
+    }
+
+    console.log(sortedTXs);
+
+    if (sortedTXs.length > 0) {
+      return sortedTXs.map((tx) => (
+        <TableRow
+          hover={true}
+          key={tx.block_signed_at + Math.random(0, 10000)}
+          style={{ cursor: "pointer" }}
+          onClick={() => this.nav("/short/detective/" + tx.contract_address)}
+        >
+          <TableCell>
+            {tx.transfer_type === "IN" && (
+              <IconButton color="primary">
+                <KeyboardArrowRightRoundedIcon />
+              </IconButton>
+            )}
+            {tx.transfer_type === "OUT" && (
+              <IconButton color="secondary">
+                <KeyboardArrowLeftRoundedIcon />
+              </IconButton>
+            )}
+          </TableCell>
+          <TableCell>
+            <Typography>{tx.dateFormated}</Typography>
+          </TableCell>
+          <TableCell padding="none" align="left">
+            <img className={classes.tokenLogo} alt="" src={tx.logo_url} />
+            <Typography variant={"h4"}>{tx.contract_name}</Typography>
+          </TableCell>
+        </TableRow>
+      ));
+    }
+  };
+
   render() {
     const { classes, t } = this.props;
     const {
@@ -437,46 +554,171 @@ class Transactions extends Component {
       walletNicknameModal,
       deleteWalletModal,
       error,
+      portfolioData,
+      sortBy,
+      sortOrder,
     } = this.state;
 
     return (
       <div className={classes.root}>
         {!account.address && <div>CONNECT WALLET</div>}
         {account.address && (
-          <Grid container>
+          <>
             {error && (
-              <Grid item xs={12} style={{ textAlign: "center" }}>
-                <Typography inline variant={"h4"}>
-                  Portfolio dashboard in development
-                </Typography>
-                <Typography inline variant={"h4"}>
-                  Please try again later. Sorry!
-                </Typography>
-              </Grid>
+              <Card className={classes.favCard} elevation={3}>
+                <Grid item xs={12} style={{ textAlign: "center" }}>
+                  <Typography inline variant={"h4"}>
+                    Portfolio dashboard in development
+                  </Typography>
+                  <Typography inline variant={"h4"}>
+                    Please try again later. Sorry!
+                  </Typography>
+                </Grid>
+              </Card>
             )}
             {!error && !dbDataLoaded && (
-              <Grid item xs={12} style={{ textAlign: "center" }}>
-                <Typography variant={"h4"}>Please give us a moment</Typography>
-                <Typography variant={"h4"}>
-                  while we prepare your portfolio data...
-                </Typography>
-                <Typography style={{ marginTop: "10px" }} variant={"h4"}>
-                  (The first time on a wallet with lots of transactions
-                </Typography>
-                <Typography variant={"h4"}>
-                  might take a couple of minutes to complete)
-                </Typography>
-                <LinearProgress style={{ marginTop: "10px" }} />
-              </Grid>
-            )}
-            {dbDataLoaded && (
-              <>
-                <Grid item xs={9}>
-                  Aca van las TX
+              <Card className={classes.favCard} elevation={3}>
+                <Grid item xs={12} style={{ textAlign: "center" }}>
+                  <Typography variant={"h4"}>
+                    Please give us a moment
+                  </Typography>
+                  <Typography variant={"h4"}>
+                    while we prepare your portfolio data...
+                  </Typography>
+                  <Typography style={{ marginTop: "10px" }} variant={"h4"}>
+                    (The first time on a wallet with lots of transactions
+                  </Typography>
+                  <Typography variant={"h4"}>
+                    might take a couple of minutes to complete)
+                  </Typography>
+                  <LinearProgress style={{ marginTop: "10px" }} />
                 </Grid>
-                <Grid item xs={3}>
-                  <Card className={classes.favCard} elevation={3}>
-                    <Grid item style={{ minWidth: "100%" }}>
+              </Card>
+            )}
+          </>
+        )}
+        {account.address && dbDataLoaded && (
+          <>
+            <Grid item xs={9}>
+              <Card className={classes.favCard} elevation={3}>
+                <TableContainer size="small">
+                  <Table className={classes.table} aria-label="assetList">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell
+                          style={{ width: "30px", height: "30px" }}
+                          align="left"
+                          padding="none"
+                        >
+                          <TableSortLabel
+                            active={sortBy === "transfer_type"}
+                            direction={sortOrder}
+                            onClick={() => this.sortBy("transfer_type")}
+                          >
+                            Type
+                          </TableSortLabel>
+                        </TableCell>
+                        <TableCell align="left" padding="none">
+                          <TableSortLabel
+                            active={sortBy === "block_signed_at"}
+                            direction={sortOrder}
+                            onClick={() => this.sortBy("block_signed_at")}
+                          >
+                            Date
+                          </TableSortLabel>
+                        </TableCell>
+                        <TableCell align="left" padding="none">
+                          <TableSortLabel
+                            active={sortBy === "contract_name"}
+                            direction={sortOrder}
+                            onClick={() => this.sortBy("contract_name")}
+                          >
+                            Name
+                          </TableSortLabel>
+                        </TableCell>
+                        <TableCell align="right">
+                          <TableSortLabel
+                            active={sortBy === "amount"}
+                            direction={sortOrder}
+                            onClick={() => this.sortBy("amount")}
+                          >
+                            Amount
+                          </TableSortLabel>
+                        </TableCell>
+                        <TableCell align="right">
+                          <TableSortLabel
+                            active={sortBy === "rate_at_tx"}
+                            direction={sortOrder}
+                            onClick={() => this.sortBy("rate_at_tx")}
+                          >
+                            Price
+                          </TableSortLabel>
+                        </TableCell>
+                        <TableCell align="right">
+                          <TableSortLabel
+                            active={sortBy === "worth_at_tx"}
+                            direction={sortOrder}
+                            onClick={() => this.sortBy("worth_at_tx")}
+                          >
+                            Worth
+                          </TableSortLabel>
+                        </TableCell>
+                        <TableCell align="right">
+                          <TableSortLabel
+                            active={sortBy === "profit_percent"}
+                            direction={sortOrder}
+                            onClick={() => this.sortBy("profit_percent")}
+                          >
+                            Profit
+                          </TableSortLabel>
+                        </TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {this.drawTransactions(portfolioData[0])}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Card>
+            </Grid>
+            <Grid item xs={3}>
+              <Card className={classes.favCard} elevation={3}>
+                <Grid item style={{ minWidth: "100%" }}>
+                  <Grid
+                    item
+                    container
+                    direction="row"
+                    justify="space-between"
+                    alignItems="center"
+                    xs={12}
+                  >
+                    <Typography variant={"h4"}>Wallets</Typography>
+                    {!addWallet && (
+                      <Button
+                        startIcon={<AddCircleRoundedIcon />}
+                        variant="outlined"
+                        size="small"
+                        color="primary"
+                        onClick={this.toggleAddWallet}
+                      >
+                        New
+                      </Button>
+                    )}
+                    {addWallet && (
+                      <Button
+                        startIcon={<ArrowBackIosRoundedIcon />}
+                        variant="contained"
+                        size="small"
+                        color="secondary"
+                        onClick={this.toggleAddWallet}
+                      >
+                        Back
+                      </Button>
+                    )}
+                  </Grid>
+                  {addWallet && (
+                    <>
+                      <Divider style={{ marginTop: 10 }} />
                       <Grid
                         item
                         container
@@ -485,82 +727,45 @@ class Transactions extends Component {
                         alignItems="center"
                         xs={12}
                       >
-                        <Typography variant={"h4"}>Wallets</Typography>
-                        {!addWallet && (
+                        <Grid item xs={9}>
+                          <TextField
+                            className={classes.walletInput}
+                            id="walletAdd"
+                            label="Wallet Address"
+                            onChange={this.handleChange}
+                            helperText={this.state.errMsgWallet}
+                            error={this.state.errorWallet}
+                          />
+                        </Grid>
+                        <Grid item style={{ textAlign: "end" }} xs={3}>
                           <Button
-                            startIcon={<AddCircleRoundedIcon />}
                             variant="outlined"
                             size="small"
                             color="primary"
-                            onClick={this.toggleAddWallet}
+                            className={classes.button}
+                            startIcon={<AddCircleRoundedIcon />}
+                            onClick={() => {
+                              this.addWallet(newWallet);
+                            }}
+                            disabled={this.state.errorWallet}
                           >
-                            New
+                            Add
                           </Button>
-                        )}
-                        {addWallet && (
-                          <Button
-                            startIcon={<ArrowBackIosRoundedIcon />}
-                            variant="contained"
-                            size="small"
-                            color="secondary"
-                            onClick={this.toggleAddWallet}
-                          >
-                            Back
-                          </Button>
-                        )}
+                        </Grid>
                       </Grid>
-                      {addWallet && (
-                        <>
-                          <Divider style={{ marginTop: 10 }} />
-                          <Grid
-                            item
-                            container
-                            direction="row"
-                            justify="space-between"
-                            alignItems="center"
-                            xs={12}
-                          >
-                            <Grid item xs={9}>
-                              <TextField
-                                className={classes.walletInput}
-                                id="walletAdd"
-                                label="Wallet Address"
-                                onChange={this.handleChange}
-                                helperText={this.state.errMsgWallet}
-                                error={this.state.errorWallet}
-                              />
-                            </Grid>
-                            <Grid item style={{ textAlign: "end" }} xs={3}>
-                              <Button
-                                variant="outlined"
-                                size="small"
-                                color="primary"
-                                className={classes.button}
-                                startIcon={<AddCircleRoundedIcon />}
-                                onClick={() => {
-                                  this.addWallet(newWallet);
-                                }}
-                                disabled={this.state.errorWallet}
-                              >
-                                Add
-                              </Button>
-                            </Grid>
-                          </Grid>
-                        </>
-                      )}
-                      <List
-                        className={classes.walletList}
-                        component="nav"
-                        aria-label="user wallet list"
-                      >
-                        {this.userWalletList(userWallets)}
-                      </List>
-                    </Grid>
-                  </Card>
+                    </>
+                  )}
+                  <List
+                    className={classes.walletList}
+                    component="nav"
+                    aria-label="user wallet list"
+                  >
+                    {this.userWalletList(userWallets)}
+                  </List>
                 </Grid>
-              </>
-            )}
-          </Grid>
+              </Card>
+            </Grid>
+          </>
         )}
         {walletNicknameModal && this.renderModal()}
         {deleteWalletModal && this.renderWalletDeleteModal()}
